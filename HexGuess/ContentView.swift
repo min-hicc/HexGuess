56c5a568bd7eecbdaa3e801641444839ff934dda
Sharon <jsmoon883@gmail.com>
2025-11-03 17:16:08 +0100

    Initial Commit

---
+++ HexGuess/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HexGuess
+//
+//  Created by Sharon Moon on 11/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HexGuess/HexGuessApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexGuessApp.swift
+//  HexGuess
+//
+//  Created by Sharon Moon on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct HexGuessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
